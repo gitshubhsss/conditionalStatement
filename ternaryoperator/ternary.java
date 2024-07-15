9a01b7ec668fadf39fdb41b406ab8edb97cf15da
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-15 17:30:42 +0530

    ternary operator in java

---
+++ ternaryoperator/ternary.java
@@ -0,0 +1,13 @@
+public class ternary {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+
+        boolean larger=(a>b)?true:false;
+
+        System.out.println(larger);
+
+        String type=(5%2==0)?"Even":"Odd";
+        System.err.println(type);
+    }
+}
