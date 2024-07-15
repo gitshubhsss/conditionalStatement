2c5f54e30986adf2e6b07e53c254ec87c3304783
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-15 17:34:18 +0530

    .\switchState.java

---
+++ switchStatement/switchState.java
@@ -0,0 +1,20 @@
+public class switchState {
+    public static void main(String[] args) {
+        int number=2;
+        switch (number) {
+            case 1:
+                System.out.println("Samosa");
+                break;
+        
+            case 2:
+                System.out.println("vadapav");
+                break;
+
+            case 3:
+                System.out.println("Chicken Biryani");
+                break;
+            default:
+                break;
+        }
+    }
+}
